ed331cf6766f001c7a7776633b097c88188f5a88
Daniel Andres Pinto <Dapinto7@misena.edu.co>
2017-11-17 23:09:10 -0500

    Movimiento

--- jugador/scr_mario.gd
+++ jugador/scr_mario.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 var GRAVEDAD = 200
 var Velocidad = Vector2()
+var vel_lateral = 50
 
 func _ready():
 	set_fixed_process(true)
@@ -10,5 +11,13 @@ func _ready():
 
 func  _fixed_process(delta):
 	Velocidad.y += GRAVEDAD * delta
+
+	if Input.is_action_pressed('ui_right'):
+		Velocidad.x += vel_lateral
+	elif Input.is_action_pressed('ui_left'):
+		Velocidad.x -= vel_lateral
+	else:
+		Velocidad.x = 0
+
 	var movimiento = Velocidad * delta
 	move(movimiento)
