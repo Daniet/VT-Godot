b91e1af84b3bb1d08eb94de43f364f84e6fe5332
Dan Dy <danie7h@gmail.com>
2017-11-19 08:34:07 -0500

    Movimiento animado

--- jugador/scr_mario.gd
+++ jugador/scr_mario.gd
@@ -5,18 +5,27 @@ var Velocidad = Vector2()
 export var vel_lateral = 50
 export var vel_max = 150
 
+var anim_play
+
 func _ready():
 	set_fixed_process(true)
 	Velocidad.x = 0
 	Velocidad.y = 0
+	anim_play = get_node('AnimationPlayer')
 
 func  _fixed_process(delta):
 	Velocidad.y += GRAVEDAD * delta
 
 	if Input.is_action_pressed('ui_right'):
 		Velocidad.x += vel_lateral
+		if !anim_play.is_playing():
+			anim_play.play('mover')
+		get_node('spr_Mario').set_flip_h(false)
 	elif Input.is_action_pressed('ui_left'):
 		Velocidad.x -= vel_lateral
+		if !anim_play.is_playing():
+			anim_play.play('mover')
+		get_node('spr_Mario').set_flip_h(true)
 	else:
 		Velocidad.x = 0
 
